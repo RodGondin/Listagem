292d27c231af02bad82ce53d1b0a2f08fee60597
Rodrigo Gondin <rodrigo.gevaerd@compasso.com.br>
2021-10-15 01:34:18 -0300

    Projeto Finalizado

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,13 +1,26 @@
-import {
-  DropdownButton,
-  Dropdown,
-  ButtonGroup,
-  FormGroup,
-  InputGroup,
-  Form,
-} from "react-bootstrap";
+import { DropdownButton, Dropdown, FormGroup, Form } from "react-bootstrap";
+import { LinesPerPage } from "./MainTable";
 
-export default function DropDown() {
+export default function DropDown({
+  clientFilter,
+  setClientFilter,
+  clientNumberFilter,
+  setClientNumberFilter,
+  qtdChargebackFilter,
+  setQtdChargebackFilter,
+  valorChargebackFilter,
+  setValorChargebackFilter,
+  qtdVendasFilter,
+  setQtdVendasFilter,
+  valorTotalFilter,
+  setValorTotalFilter,
+  chargebackFilter,
+  setChargebackFilter,
+  totalChargebackFilter,
+  setTotalChargebackFilter,
+  setLinesPerPage,
+  linesPerPage,
+}) {
   return (
     <Dropdown>
       <DropdownButton
@@ -18,45 +31,115 @@ export default function DropDown() {
       >
         <Dropdown.ItemText id="filters">Linhas por página</Dropdown.ItemText>
         <FormGroup>
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="row"
+            value="padrão"
+            onChange={() => {
+              setLinesPerPage(LinesPerPage.DEFAULT);
+            }}
+            checked={linesPerPage === LinesPerPage.DEFAULT}
+          />
           <Form.Check.Label id="checklabel">Padrão</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" />
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="row"
+            value="50linhas"
+            onChange={() => {
+              setLinesPerPage(LinesPerPage.MAX);
+            }}
+            checked={linesPerPage === LinesPerPage.MAX}
+          />
           <Form.Check.Label id="checklabel">50 linhas</Form.Check.Label>
           <br />
 
           <Dropdown.Divider />
-
           <Dropdown.ItemText id="filters">Colunas</Dropdown.ItemText>
 
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setClientNumberFilter(!clientNumberFilter)}
+            checked={clientNumberFilter}
+          />
           <Form.Check.Label id="checklabel">Nº Cliente</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setClientFilter(!clientFilter)}
+            checked={clientFilter}
+          />
           <Form.Check.Label id="checklabel">Cliente</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setQtdChargebackFilter(!qtdChargebackFilter)}
+            checked={qtdChargebackFilter}
+          />
           <Form.Check.Label id="checklabel">Qtd Chargeback</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setValorChargebackFilter(!valorChargebackFilter)}
+            checked={valorChargebackFilter}
+          />
           <Form.Check.Label id="checklabel">Valor Chargeback</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setQtdVendasFilter(!qtdVendasFilter)}
+            checked={qtdVendasFilter}
+          />
           <Form.Check.Label id="checklabel">Qtd Vendas</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setValorTotalFilter(!valorTotalFilter)}
+            checked={valorTotalFilter}
+          />
           <Form.Check.Label id="checklabel">Valor Total</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setChargebackFilter(!chargebackFilter)}
+            checked={chargebackFilter}
+          />
           <Form.Check.Label id="checklabel">% Chargeback</Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
+
+          <Form.Check.Input
+            id="checkinput"
+            type="checkbox"
+            name="column"
+            onChange={() => setTotalChargebackFilter(!totalChargebackFilter)}
+            checked={totalChargebackFilter}
+          />
           <Form.Check.Label id="checklabel">
             % Total Chargeback
           </Form.Check.Label>
           <br />
-          <Form.Check.Input id="checkinput" type="checkbox" checked />
-          <Form.Check.Label id="checklabel">Status</Form.Check.Label>
         </FormGroup>
       </DropdownButton>
     </Dropdown>
--- src/components/MainTable.tsx
+++ src/components/MainTable.tsx
@@ -1,69 +1,163 @@
 import { Table } from "react-bootstrap";
 import DropDown from "./DropDown";
 
+import { useState, useEffect } from "react";
+
+export interface Cliente {
+  id: string;
+  fullName: string;
+  qtdChargeback: number;
+  valueChargeback: number;
+  salesAmount: number;
+  totalValue: number;
+  chargebackPercentage: number;
+  chargebackTotal: number;
+}
+
+export enum LinesPerPage {
+  DEFAULT = 5,
+  MAX = 50,
+}
+
 export default function MainTable() {
+  const [clientNumberFilter, setClientNumberFilter] = useState<boolean>(true);
+  const [clientFilter, setClientFilter] = useState<boolean>(true);
+  const [qtdChargebackFilter, setQtdChargebackFilter] = useState<boolean>(true);
+  const [valorChargebackFilter, setValorChargebackFilter] =
+    useState<boolean>(true);
+  const [qtdVendasFilter, setQtdVendasFilter] = useState<boolean>(true);
+  const [valorTotalFilter, setValorTotalFilter] = useState<boolean>(true);
+  const [chargebackFilter, setChargebackFilter] = useState<boolean>(true);
+  const [totalChargebackFilter, setTotalChargebackFilter] =
+    useState<boolean>(true);
+  const [linesPerPage, setLinesPerPage] = useState<LinesPerPage>(
+    LinesPerPage.DEFAULT
+  );
+
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+
+  useEffect(() => {
+    const list: Cliente[] = [];
+    for (let i = 0; i < 50; i++) {
+      list.push(
+        {
+          id: "0001",
+          fullName: "Pedro Antônio",
+          qtdChargeback: 1,
+          valueChargeback: 38.92,
+          salesAmount: 72,
+          totalValue: 32000,
+          chargebackPercentage: 0.05,
+          chargebackTotal: 0.03,
+        },
+        {
+          id: "0002",
+          fullName: "Antônio Carlos",
+          qtdChargeback: 1,
+          valueChargeback: 38.92,
+          salesAmount: 17,
+          totalValue: 17000,
+          chargebackPercentage: 0.05,
+          chargebackTotal: 0.03,
+        },
+        {
+          id: "0003",
+          fullName: "Carlos Pedro",
+          qtdChargeback: 1,
+          valueChargeback: 38.92,
+          salesAmount: 8,
+          totalValue: 5000,
+          chargebackPercentage: 0.05,
+          chargebackTotal: 0.03,
+        }
+      );
+    }
+    setClientes(list);
+  }, []);
+
   return (
     <Table striped bordered hover borderless>
       <thead>
         <tr style={{ color: "#458097" }}>
-          <th style={{ width: 150 }}>Nº CLIENTE</th>
-          <th style={{ width: 250 }}>CLIENTE</th>
-          <th id="middle" style={{ width: 180 }}>
-            QTD. CHARGEBACK
-          </th>
-          <th>VALOR CHARGEBACK</th>
-          <th id="middle">QTD VENDAS</th>
-          <th style={{ width: 180 }}>VALOR TOTAL</th>
-          <th id="middle">% CHARGEBACK</th>
-          <th id="middle">% TOTAL CHARGEBACK</th>
+          {clientNumberFilter ? (
+            <th style={{ width: 150 }}>Nº CLIENTE</th>
+          ) : null}
+
+          {clientFilter ? <th style={{ width: 250 }}>CLIENTE</th> : null}
+
+          {qtdChargebackFilter ? (
+            <th id="middle" style={{ width: 180 }}>
+              QTD. CHARGEBACK
+            </th>
+          ) : null}
+
+          {valorChargebackFilter ? <th>VALOR CHARGEBACK</th> : null}
+
+          {qtdVendasFilter ? <th id="middle">QTD VENDAS</th> : null}
+
+          {valorTotalFilter ? (
+            <th style={{ width: 180 }}>VALOR TOTAL</th>
+          ) : null}
+
+          {chargebackFilter ? <th id="middle">% CHARGEBACK</th> : null}
+
+          {totalChargebackFilter ? (
+            <th id="middle">% TOTAL CHARGEBACK</th>
+          ) : null}
+
           <th id="end">
-            <DropDown />
+            <DropDown
+              setClientNumberFilter={setClientNumberFilter}
+              clientNumberFilter={clientNumberFilter}
+              clientFilter={clientFilter}
+              setClientFilter={setClientFilter}
+              qtdChargebackFilter={qtdChargebackFilter}
+              setQtdChargebackFilter={setQtdChargebackFilter}
+              valorChargebackFilter={valorChargebackFilter}
+              setValorChargebackFilter={setValorChargebackFilter}
+              qtdVendasFilter={qtdVendasFilter}
+              setQtdVendasFilter={setQtdVendasFilter}
+              valorTotalFilter={valorTotalFilter}
+              setValorTotalFilter={setValorTotalFilter}
+              chargebackFilter={chargebackFilter}
+              setChargebackFilter={setChargebackFilter}
+              totalChargebackFilter={totalChargebackFilter}
+              setTotalChargebackFilter={setTotalChargebackFilter}
+              setLinesPerPage={setLinesPerPage}
+              linesPerPage={linesPerPage}
+            />
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>0001</td>
-          <td>Pedro Antonio</td>
-          <td id="middle">1</td>
-          <td>R$ 38,92</td>
-          <td id="middle">72</td>
-          <td>R$32.000</td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.05%
-          </td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.03%
-          </td>
-        </tr>
-        <tr>
-          <td>0002</td>
-          <td>Rogerio Flores</td>
-          <td id="middle">1</td>
-          <td>R$ 38,92</td>
-          <td id="middle">58</td>
-          <td>R$17.500</td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.05%
-          </td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.03%
-          </td>
-        </tr>
-        <tr>
-          <td>0003</td>
-          <td>Francisco Coelho</td>
-          <td id="middle">1</td>
-          <td>R$ 38,92</td>
-          <td id="middle">5</td>
-          <td>R$1.500</td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.05%
-          </td>
-          <td id="middle" style={{ color: "#c73d2bdf" }}>
-            ↝ 0.03%
-          </td>
-        </tr>
+        {clientes.slice(0, linesPerPage).map((cliente, index) => {
+          return (
+            <tr key={index}>
+              {clientNumberFilter ? <td>{cliente.id}</td> : null}
+              {clientFilter ? <td>{cliente.fullName}</td> : null}
+              {qtdChargebackFilter ? (
+                <td id="middle">{cliente.qtdChargeback}</td>
+              ) : null}
+              {valorChargebackFilter ? (
+                <td>R$ {cliente.valueChargeback}</td>
+              ) : null}
+              {qtdVendasFilter ? (
+                <td id="middle">{cliente.salesAmount}</td>
+              ) : null}
+              {valorTotalFilter ? <td>R$ {cliente.totalValue}</td> : null}
+              {chargebackFilter ? (
+                <td id="middle" style={{ color: "#c73d2bdf" }}>
+                  ↝ {cliente.chargebackPercentage}%
+                </td>
+              ) : null}
+              {totalChargebackFilter ? (
+                <td id="middle" style={{ color: "#c73d2bdf" }}>
+                  ↝ {cliente.chargebackTotal}%
+                </td>
+              ) : null}
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
